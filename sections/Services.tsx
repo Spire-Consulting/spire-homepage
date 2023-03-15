5e1f6038c55e8dac0e32a6d47ee24e105a8a2739
quarc0o <emmanuelq150@gmail.com>
2023-03-15 19:03:39 +0100

    Work on about us page
    Made the icon sections line up horizontally as in design, started work
    mobile version

--- sections/Services.tsx
+++ sections/Services.tsx
@@ -16,17 +16,21 @@ const services = {
 
 export default function Services({ title, description, type }: ServicesProps) {
     return (
-        <div className={"relative flex flex-col mb-16"}>
+        <div className={"relative flex flex-col mb-16 sm:bg-red-500 md:bg-blue-500"}>
             <div className={"flex mb-4 place-content-center"}>
                 <HeaderWithTriangle title={title} color="lime" />
             </div>
             <p className="flex place-content-center">{description}</p>
-            <div className="flex justify-between mt-16">
+            <div className="flex flex-col sm:flex-row sm:justify-between gap-16 mt-16">
                 {services[type].map((service: Service, index: number) => (
                     <div key={index} className="flex flex-col items-center gap-1">
                         {service.icon}
-                        <h4>{service.title}</h4>
-                        <p className="text-xs mt-2 text-center">Dette innebærer noe slik. litt mer tekst for å se hvordan det kan se ut</p>
+                        <div className="flex flex-col items-center w-48">
+                            <div className="border-solid border-lime border-b-2 w-48 flex justify-center">
+                                <h4 className="no-underline">{service.title}</h4>
+                            </div>
+                            <p className="text-xs mt-2 text-center">Dette innebærer noe slik. litt mer tekst for å se hvordan det kan se ut</p>
+                        </div>
                     </div>
                 ))}
             </div>
