906d6a91ea9f0769f54b3baf271e745343e91c73
unknown <hermanrql@gmail.com>
2023-03-15 18:59:57 +0100

    fixed footer

--- sections/ContactInfo.tsx
+++ sections/ContactInfo.tsx
@@ -4,37 +4,14 @@ import Link from "next/link"
 import Mail from "../components/buttons/Mail"
 
 type ContactInfoProps = {
-    withImage: boolean
+    children: JSX.Element
 }
 
-export default function ContactInfo({ withImage }: ContactInfoProps) {
+export default function ContactInfo({ children }: ContactInfoProps) {
     return (
-        <div className="flex flex-col space-y-2 text-slate-300">
-            {withImage && (
-                <>
-                    <Image
-                        src="/images/logo_spireconsulting_hvit_NOBG.svg"
-                        alt="logo"
-                        width="150"
-                        height="150"
-                    />
-                    <span className="w-full h-[3px] bg-lightBlue !mt-0" />
-                </>
-            )}
-            <div className="flex items-center">
-                <Mail email="kontakt@spireconsulting.no" />
-                <Link href="mailto:kontakt@spireconsulting.no" className="ml-6 ">
-                    kontakt@spireconsulting.no
-                </Link>{" "}
-            </div>
-            <div className="flex items-center ">
-                <PhoneIcon className="h-6 " />
-                <p className="ml-6 text-slate-300">+47 480 00 508</p>
-            </div>
-            <div className="flex items-center ">
-                <HomeIcon className="h-6 " />
-                <p className="ml-6 text-slate-300">Org nr: 916 672 357</p>
-            </div>
+        <div className="flex flex-col space-y-2 w-56 py-8 md:py-0">
+            <div className="w-full bg-lightBlue h-px"></div>
+            {children}
         </div>
     )
 }
